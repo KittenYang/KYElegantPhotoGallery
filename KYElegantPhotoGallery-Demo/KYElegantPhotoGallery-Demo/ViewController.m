1eb3e02311ab6663164d65b0ecb54b9687396cb5
KittenYang <kittenyang@icloud.com>
2015-05-31 00:34:14 +0800

    first commit

---
+++ KYElegantPhotoGallery-Demo/KYElegantPhotoGallery-Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KYElegantPhotoGallery-Demo
+//
+//  Created by Kitten Yang on 5/31/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
